65dcc8cf93a5aaf050bc9aed221042a60debc01e
Richard <richardvdv105@gmail.com>
2020-10-27 13:40:11 +0100

    Fix

--- lib/app/homepage.dart
+++ lib/app/homepage.dart
@@ -18,7 +18,7 @@ class _QrCodeState extends State<Homepage> {
 
   _scan() async {
     await FlutterBarcodeScanner.scanBarcode(
-            "#000000", "Cancel", true, ScanMode.BARCODE)
+        "#000000", "Cancel", true, ScanMode.BARCODE)
         .then((value) => setState(() => _data = value));
 
     //open AlertDialog
@@ -33,86 +33,92 @@ class _QrCodeState extends State<Homepage> {
         body: SingleChildScrollView(
             child: Form(
                 child: Column(children: <Widget>[
-      SizedBox(height: 20),
-      Text('EzCharge',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36.0),
-          textAlign: TextAlign.center),
+                  SizedBox(height: 20),
+                  Text('EzCharge',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 36.0),
+                      textAlign: TextAlign.center),
 
-      SizedBox(height: 20),
-      Text(
-          'This is a placeholder description.\n'
-          'When we think of a good description it will go here.',
-          style: TextStyle(fontSize: 20),
-          textAlign: TextAlign.center),
+                  SizedBox(height: 20),
+                  Text(
+                      'This is a placeholder description.\n'
+                          'When we think of a good description it will go here.',
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center),
 
-      SizedBox(height: 30),
-      Text('Most recent sessions',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
-          textAlign: TextAlign.center),
-      Table(
-        border: TableBorder.all(width: 1.0, color: Colors.black),
-        children: [
-          TableRow(children: [
-            TableCell(
-                child: Center(
-                    child: Text('\nDate\n',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16)))),
-            TableCell(
-                child: Center(
-                    child: Text('\nLocation\n',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16)))),
-            TableCell(
-                child: Center(
-                    child: Text('\nTime Charged\n',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16))))
-          ]),
-          TableRow(children: [
-            TableCell(child: Center(child: Text('\nCell 1\n'))),
-            TableCell(child: Center(child: Text('\nCell 2\n'))),
-            TableCell(child: Center(child: Text('\nCell 3\n')))
-          ]),
-          TableRow(children: [
-            TableCell(child: Center(child: Text('\nCell 4\n'))),
-            TableCell(child: Center(child: Text('\nCell 5\n'))),
-            TableCell(child: Center(child: Text('\nCell 6\n')))
-          ]),
-          TableRow(children: [
-            TableCell(child: Center(child: Text('\nCell 7\n'))),
-            TableCell(child: Center(child: Text('\nCell 8\n'))),
-            TableCell(child: Center(child: Text('\nCell 9\n')))
-          ]),
-        ],
-      ),
+                  SizedBox(height: 30),
+                  Text('Most recent sessions',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 24.0),
+                      textAlign: TextAlign.center),
+                  Table(
+                    border: TableBorder.all(width: 1.0, color: Colors.black),
+                    children: [
+                      TableRow(children: [
+                        TableCell(
+                            child: Center(
+                                child: Text('\nDate\n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16)))),
+                        TableCell(
+                            child: Center(
+                                child: Text('\nLocation\n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16)))),
+                        TableCell(
+                            child: Center(
+                                child: Text('\nTime Charged\n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16))))
+                      ]),
+                      TableRow(children: [
+                        TableCell(child: Center(child: Text('\nCell 1\n'))),
+                        TableCell(child: Center(child: Text('\nCell 2\n'))),
+                        TableCell(child: Center(child: Text('\nCell 3\n')))
+                      ]),
+                      TableRow(children: [
+                        TableCell(child: Center(child: Text('\nCell 4\n'))),
+                        TableCell(child: Center(child: Text('\nCell 5\n'))),
+                        TableCell(child: Center(child: Text('\nCell 6\n')))
+                      ]),
+                      TableRow(children: [
+                        TableCell(child: Center(child: Text('\nCell 7\n'))),
+                        TableCell(child: Center(child: Text('\nCell 8\n'))),
+                        TableCell(child: Center(child: Text('\nCell 9\n')))
+                      ]),
+                    ],
+                  ),
 
-      SizedBox(height: 20),
-      Text('Start session',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36.0),
-          textAlign: TextAlign.center),
+                  SizedBox(height: 20),
+                  Text('Start session',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 36.0),
+                      textAlign: TextAlign.center),
 
-      IconButton(
-        icon: Icon(Icons.qr_code_scanner_rounded, size: 150),
-        alignment: Alignment.center,
-        padding: EdgeInsets.only(right: 125),
-        onPressed: () {
-          if (sessionStarted) {
-            stopSession(docRef);
-          } else {
-            _scan();
-          }
-        },
-        //Open QR code scanner
-      ),
+                  IconButton(
+                    icon: Icon(Icons.qr_code_scanner_rounded, size: 150),
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.only(right: 125),
+                    onPressed: () {
+                      if (sessionStarted) {
+                        stopSession(docRef);
+                      } else {
+                        _scan();
+                      }
+                    },
+                    //Open QR code scanner
+                  ),
 
-      SizedBox(height: 90),
-      Text('Requires camera')
-      // THIS LINE IS REQUIRED
-      // FOR SOME REASON ICONS ARE NOT SEEN AS FILLING
-      // MEANING THAT WHEN YOU PUT THE PHONE SIDEWAYS
-      // IT WONT SCROLL ALL THE WAY DOWN WITHOUT THIS.
-    ]))));
+                  SizedBox(height: 90),
+                  Text('Requires camera')
+                  // THIS LINE IS REQUIRED
+                  // FOR SOME REASON ICONS ARE NOT SEEN AS FILLING
+                  // MEANING THAT WHEN YOU PUT THE PHONE SIDEWAYS
+                  // IT WONT SCROLL ALL THE WAY DOWN WITHOUT THIS.
+                ]))));
   }
 
   Future<void> _showMyDialog() async {
@@ -141,6 +147,7 @@ class _QrCodeState extends State<Homepage> {
               child: Text('Yes'),
               onPressed: () async {
                 docRef = await startSession();
+                print(docRef);
               },
             ),
           ],
@@ -163,6 +170,7 @@ class _QrCodeState extends State<Homepage> {
     Navigator.of(context).pop();
     sessionStarted = true;
     return docRef;
+
   }
 
   stopSession(docRef) {
