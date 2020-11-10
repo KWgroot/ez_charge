cd5ea3c845f6e02d0c4480bdb6a4abf12865a5d4
Richard <richvdv105@gmail.com>
2020-11-10 14:15:02 +0100

    Start instant app screen

--- lib/instant_app/instant_app.dart
+++ lib/instant_app/instant_app.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     handleDynamicLinks(context);
-  return MaterialApp(
+    return MaterialApp(
       title: 'EZCharge',
       home: Scaffold(
         appBar: AppBar(
@@ -38,76 +38,24 @@ class MyApp extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 //Registration Form Text
-                Text('Registration Form',
+                Text('Charge your car',
                     style:
                     TextStyle(fontWeight: FontWeight.bold, fontSize: 36.0),
                     textAlign: TextAlign.center),
 
                 //Page Description Text
-                Text('Fill in the fields below to create an account.',
-                    style: TextStyle(fontSize: 20.0),
-                    textAlign: TextAlign.center),
 
-                // Edit text field (Email)
-                SizedBox(height: 20.0),
-                TextFormField(
-                    decoration: new InputDecoration(hintText: 'Email'),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: validateEmail,
-                    onChanged: (val) {
-                      email = val;
-                    }),
-
-                // Edit text field (Password)
-                SizedBox(height: 20.0),
-                TextFormField(
-                    decoration: new InputDecoration(hintText: 'Password'),
-                    obscureText: true,
-                    validator: validatePassword,
-                    onChanged: (val) {
-                      pswd1 = val;
-                    }),
-
-                // Edit text field (Confirm Password)
-                SizedBox(height: 20.0),
-                TextFormField(
-                    decoration:
-                    new InputDecoration(hintText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: validatePassword,
-                    onChanged: (val) {
-                      pswd2 = val;
-                    }),
-
-                // SUBMIT button
                 SizedBox(height: 20.0),
                 ButtonTheme(
                     minWidth: double.infinity,
                     child: RaisedButton(
                         color: Colors.yellow[400],
                         child: Text(
-                          'SUBMIT',
+                          'Google Pay',
                           style: TextStyle(color: Colors.black),
                         ),
                         onPressed: () =>{
-                          succes = submitForm(formKey, pswd1, pswd2, email),
-                          if (succes) {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage()))
-                          }
-                        }
-                    )
-                ),
-
-                ButtonTheme(
-                    minWidth: double.infinity,
-                    child: RaisedButton(
-                        color: Colors.yellow[400],
-                        child: Text(
-                          'Skip Regi',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        onPressed: () =>{
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage()))
+                          Fluttertoast.showToast(msg: "You simulated that you have payed"),
                         }
                     )
                 ),
