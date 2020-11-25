9a485b2bd0bdc8fa29dfaca24524885b188e800b
Richard <richardvdv105@gmail.com>
2020-11-25 12:26:11 +0100

    Only need firestore data

--- lib/app/invoices.dart
+++ lib/app/invoices.dart
@@ -1,24 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../app/global_variables.dart' as globals;
 
-class Invoices extends StatelessWidget{
+class Invoices extends StatelessWidget {
 
+  var date = ['Date', 'test', 'test2'];
+  var number = ['Number' , 0 , 1];
+  var amount = ['Amount', 10.00, 11.03];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Form(
-                child: Column(
+      body: ListView.builder(
+        itemBuilder: (context, position) {
+          return Column(
+            children: <Widget>[
+              Padding(padding:
+              const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 6.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      SizedBox(height: 20),
-                      Text('Invoices',
-                          style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 36.0),
-                          textAlign: TextAlign.center),
-                    ]
-                )
-            )
-        )
+                      Container(
+                        child: Text(date[position]), alignment: Alignment(0.0, 0.0), height: 50,),
+                      Text(number[position].toString()),
+                      Text(amount[position].toString())
+                    ],
+                  )
+              ),
+              Divider(
+                height: 2.0,
+                color: Colors.grey,
+              )
+            ],
+          );
+        },
+        itemCount: 3,
+      ),
     );
   }
+}
+getInformation(){
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  firestore.collection("chargingSession").orderBy('stopTime').orderBy('startTime').orderBy('poleid').startAt(globals.user.uid);
+
 }
