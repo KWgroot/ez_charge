52ac86ef1cf9c77b506f4d1ca2213dc3d57dd3c8
Richard <richardvdv105@gmail.com>
2020-12-02 11:41:31 +0100

    merged master

--- lib/app/settings.dart
+++ lib/app/settings.dart
@@ -3,46 +3,70 @@ import 'package:ez_charge/base/homepage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import '../app/global_variables.dart' as globals;
 import 'package:firebase_core/firebase_core.dart';
 
-class Settings extends StatelessWidget{
+class Settings extends StatefulWidget {
+  @override
+  SettingsScreen createState() => SettingsScreen();
+}
+
+class SettingsScreen extends State<Settings> {
   FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    // if(globals.user.uid != ""){
+    //
+    // } else{
+    //   Navigator.of(context).pop();
+    // }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
             child: Form(
-                child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 20),
-                      Text('Settings',
-                          style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 36.0),
-                          textAlign: TextAlign.center),
-                      ButtonTheme(
-                          minWidth: double.infinity,
-                          height: 40.0,
-                          child: RaisedButton(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20)
-                              ),
-                              color: Colors.yellow[400],
-                              child: Text(
-                                  'Log out',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 20.0
-                                  )
-                              ),
-                              onPressed: () async {logOut(context);}
-                          )
-                      ),
-                    ]
-                )
-            )
-        )
-    );
+                child: Column(children: <Widget>[
+      SizedBox(height: 20),
+      Text('Settings',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36.0),
+          textAlign: TextAlign.center),
+      RaisedButton(
+          color: Colors.yellow[400],
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Text(
+            'Change Password',
+            style: TextStyle(color: Colors.black, fontSize: 20.0),
+          ),
+          onPressed: () async {
+            auth.sendPasswordResetEmail(email: globals.user.email);
+            Fluttertoast.showToast(
+                msg: "We have sent you a password reset email",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.BOTTOM,
+                timeInSecForIosWeb: 1,
+                backgroundColor: Colors.black,
+                textColor: Colors.white,
+                fontSize: 16.0);
+          }),
+      ButtonTheme(
+          minWidth: double.infinity,
+          height: 40.0,
+          child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              color: Colors.yellow[400],
+              child: Text('Log out',
+                  style: TextStyle(color: Colors.black, fontSize: 20.0)),
+              onPressed: () async {
+                logOut(context);
+              })),
+    ]))));
   }
+
   void logOut(context) async {
     return showDialog<void>(
       context: context,
